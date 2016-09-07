c397403c8a991d8ea773f82578ceb241f824484f
Sebastian Sdorra <sebastian.sdorra@triology.de>
2016-09-07 15:04:53 +0200

    added missing javadoc

--- src/main/java/com/cloudogu/wiki/WikiResources.java
+++ src/main/java/com/cloudogu/wiki/WikiResources.java
@@ -14,7 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Util methods to handle resources.
+ * 
  * @author Sebastian Sdorra
  */
 public class WikiResources {
@@ -24,18 +25,49 @@ public class WikiResources {
     private WikiResources() {
     }
 
+    /**
+     * Returns path to resource. The path is calculated relative to the {@link WikiResources} class.
+     * 
+     * @param name name of resource
+     * 
+     * @return resource path
+     */
     public static String path(String name) {
         return path(WikiResources.class, name);
     }
-    
+
+    /**
+     * Returns path to resource. The path is calculated relative to the given context class.
+     * 
+     * @param contextClass context class
+     * @param name name of resource
+     * 
+     * @return resource path
+     */
     public static String path(Class contextClass, String name) {
         return contextClass.getPackage().getName().replace('.', '/') + "/" + name;
     }
     
+    /**
+     * Calculates the path path for the resource and reads the resource. {@link WikiResources} class is used as context
+     * class.
+     * 
+     * @param name name of resource
+     * 
+     * @return content of resource
+     */
     public static String read(String name){
         return read(WikiResources.class, name);
     }
-    
+
+    /**
+     * Calculates the path path for the resource and reads the resource.
+     * 
+     * @param contextClass context class
+     * @param name name of resource
+     * 
+     * @return content of resource
+     */    
     public static String read(Class contextClass, String name){
         String path = path(contextClass, name);
         LOG.trace("load script {}", path);
