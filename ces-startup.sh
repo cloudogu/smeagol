593d182e5acbd19432377f9f9b1f907efed23099
Sebastian Sdorra <sebastian.sdorra@triology.de>
2016-09-08 16:25:13 +0200

    #5 start ces integration

---
+++ ces-startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+FQDN=$(doguctl config --global fqdn)
+export SMEAGOL_SERVICE_URL="https://${FQDN}/smeagol"
+export SMEAGOL_CAS_URL="https://${FQDN}/cas"
+export SCM_INSTANCE_URL="https://${FQDN}/scm"
+
+TRUSTSTORE="${SMEAGOL_HOME}/truststore.jks"
+create_truststore.sh "${TRUSTSTORE}" > /dev/null
+
+java -Djava.awt.headless=true \
+  -Djava.net.preferIPv4Stack=true \
+  -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
+  -Djavax.net.ssl.trustStorePassword=changeit \
+  -jar /app/smeagol-app.jar
