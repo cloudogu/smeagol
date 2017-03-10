0e3fc984953cd5b923d66568dbb6ecfe6fcc48e7
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-10 10:11:50 +0100

    refactor NotifyServlet #16

--- src/main/java/com/cloudogu/wiki/scmm/NotifyServlet.java
+++ src/main/java/com/cloudogu/wiki/scmm/NotifyServlet.java
@@ -5,19 +5,10 @@
  */
 package com.cloudogu.wiki.scmm;
 
-import com.cloudogu.wiki.Account;
 import com.cloudogu.wiki.SessionStore;
-import java.io.File;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -26,16 +17,12 @@ import org.slf4j.LoggerFactory;
  */
 public class NotifyServlet extends HttpServlet {
 
-    private final SessionStore sessions;
-    private final ScmWikiProvider provider;
-
-    private final String COMMON_START = "servlet.";
+    private final RepositoryNotification notification;
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(NotifyServlet.class);
 
     public NotifyServlet(SessionStore sessions, ScmWikiProvider provider) {
-        this.sessions = sessions;
-        this.provider = provider;
+        notification = new RepositoryNotification(sessions, provider);
     }
 
     @Override
@@ -43,51 +30,7 @@ public class NotifyServlet extends HttpServlet {
         String repositoryToUpdate = request.getParameter("id");
         LOG.info("update repository {}", repositoryToUpdate);
         if (repositoryToUpdate != null && !repositoryToUpdate.isEmpty()) {
-            updateRepository(repositoryToUpdate);
-        }
-    }
-
-    private void updateRepository(String repositoryToUpdate) {
-        Iterator<HttpSession> sessionIter = sessions.getAll();
-        Set<String> updatedDirectories = new HashSet<>();
-        while (sessionIter.hasNext()) {
-            HttpSession s = sessionIter.next();
-            checkForServletsInSession(s, updatedDirectories, repositoryToUpdate);
-        }
-    }
-
-    private void checkForServletsInSession(HttpSession session, Set<String> updatedDirectories, String repositoryToUpdate) {
-        synchronized (session) {
-            Enumeration<String> attributes = session.getAttributeNames();
-            Account account = (Account) session.getAttribute("com.cloudogu.wiki.Account");
-            while (attributes.hasMoreElements()) {
-                String attribute = attributes.nextElement();
-                checkIfAttributeIsServlet(updatedDirectories, account, repositoryToUpdate, attribute);
-            }
-        }
-
-    }
-
-    private void checkIfAttributeIsServlet(Set<String> updatedDirectories, Account account, String repositoryToUpdate, String attribute) {
-        if (attribute.startsWith(COMMON_START)) {
-            String wikiName = attribute.substring(COMMON_START.length());            
-            if (wikiName.startsWith(repositoryToUpdate) && !updatedDirectories.contains(wikiName)) {
-                updateDirectory(wikiName, updatedDirectories, account);
-            }
-        }
-    }
-
-    private void updateDirectory(String wikiName, Set<String> updatedDirectories, Account account) {
-        LOG.info("found repository {} for user {}", wikiName, account.getUsername());
-        File dir = provider.getRepositoryDirectory(wikiName);
-        String branch = provider.getDecodedBranchName(wikiName);
-        System.out.println("branch: "+branch);
-        try {
-            LOG.info("pull changes");
-            provider.pullChanges(account, dir, branch);
-            updatedDirectories.add(wikiName);
-        } catch (GitAPIException | IOException ex) {
-            LOG.warn(null, ex);
+            notification.notifyForRepository(repositoryToUpdate);
         }
     }
 }
---
+++ src/main/java/com/cloudogu/wiki/scmm/RepositoryNotification.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2016 Cloudogu GmbH. All Rights Reserved.
+ * 
+ * Copyright notice
+ */
+package com.cloudogu.wiki.scmm;
+
+import com.cloudogu.wiki.Account;
+import com.cloudogu.wiki.SessionStore;
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import javax.servlet.http.HttpSession;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Michael Behlendorf
+ */
+public class RepositoryNotification {
+    
+    private final SessionStore sessions;
+    private final ScmWikiProvider provider;
+    private String repositoryToUpdate;
+    private final Set<String> updatedDirectories = new HashSet<>();
+    private final String COMMON_START = "servlet.";
+    
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(RepositoryNotification.class);
+    
+    
+    public RepositoryNotification(SessionStore sessions, ScmWikiProvider provider) {
+        this.sessions = sessions;
+        this.provider = provider;
+    }
+    
+    public void notifyForRepository(String repositoryToUpdate){
+        this.repositoryToUpdate = repositoryToUpdate;
+        updatedDirectories.clear();
+        updateRepository();
+    }
+    
+    private void updateRepository() {
+        Iterator<HttpSession> sessionIter = sessions.getAll();
+        while (sessionIter.hasNext()) {
+            HttpSession s = sessionIter.next();
+            synchronized (s) {
+                updateWikisInSession(s);
+            }
+        }
+    }
+
+    private void updateWikisInSession(HttpSession session) {
+        Enumeration<String> attributes = session.getAttributeNames();
+        if (attributes != null) {
+            Account account = (Account) session.getAttribute("com.cloudogu.wiki.Account");
+            while (attributes.hasMoreElements()) {
+                String attribute = attributes.nextElement();
+                tryToUpdateWikiForAttribute(account, attribute);
+            }
+        }
+
+    }
+
+    private void tryToUpdateWikiForAttribute(Account account, String attribute) {
+        if (attribute.startsWith(COMMON_START)) {
+            String wikiName = attribute.substring(COMMON_START.length());
+            if (wikiRequiresUpdate(wikiName)) {
+                updateWiki(wikiName, account);
+            }
+        }
+    }
+    
+    private boolean wikiRequiresUpdate(String wikiName){
+        return wikiName.startsWith(repositoryToUpdate) && !updatedDirectories.contains(wikiName);
+    }
+
+    private void updateWiki(String wikiName, Account account) {
+        LOG.info("found repository {} for user {}", wikiName, account.getUsername());
+        File dir = provider.getRepositoryDirectory(wikiName);
+        String branch = ScmWikiProvider.getDecodedBranchName(wikiName);
+        try {
+            LOG.info("pull changes");
+            provider.pullChanges(account, dir, branch);
+            updatedDirectories.add(wikiName);
+        } catch (GitAPIException | IOException ex) {
+            LOG.warn(null, ex);
+        }
+    }
+    
+}
