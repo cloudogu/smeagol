f8edbddae0c0b979b602e181ee6938e6d54cd7d7
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-10 10:12:27 +0100

    add tests for NotifyServlet #16

--- src/test/java/com/cloudogu/wiki/NotifyServletTest.java
+++ src/test/java/com/cloudogu/wiki/NotifyServletTest.java
@@ -10,9 +10,8 @@ import com.cloudogu.wiki.scmm.ScmWikiProvider;
 import com.google.common.collect.ImmutableSet;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Enumeration;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -49,34 +48,43 @@ public class NotifyServletTest {
     @Mock
     private HttpSession session;
     
+    @Mock
+    private HttpSession session2;
+    
     
     NotifyServlet servlet;
     
     
-    
     private final static String REPOSITORY = "TestRepo";
+    private final static String WRONG_REPOSITORY = "Wrong";
     private final static String BRANCH = "TestBranch";
+    private final static String BRANCH2 = "TestBranch2";
     private final static String ACCOUNT = "com.cloudogu.wiki.Account";
     private Account account;
             
     @Before
     public void setUpMocks(){
         account = new Account("name", "displayName", "mail");
-        List<HttpSession> sessions = Arrays.asList(session);
-        
-        servlet = new NotifyServlet(sessionStore, wikiProvider);
-
-        when(sessionStore.getAll()).thenReturn(sessions.iterator());
+        Set<HttpSession> sessions = new HashSet<>();
+        sessions.add(session);
+        sessions.add(session2);
         
         Set<String> attributes = ImmutableSet.of(ACCOUNT, "servlet."+REPOSITORY+"/"+BRANCH);
         Enumeration<String> attributesEnum = java.util.Collections.enumeration(attributes);
         when(session.getAttributeNames()).thenReturn(attributesEnum);
         when(session.getAttribute(ACCOUNT)).thenReturn(account);
         
+        servlet = new NotifyServlet(sessionStore, wikiProvider);
+
+        when(sessionStore.getAll()).thenReturn(sessions.iterator());
+        
     }
     
+    /*
+    * tests with one servlet entry
+    */
     @Test
-    public void testDoGet() throws IOException, ServletException, GitAPIException {
+    public void testNotifyGet() throws IOException, ServletException, GitAPIException {
         when(request.getParameter("id")).thenReturn(REPOSITORY);
         when(request.getMethod()).thenReturn("GET");
         
@@ -87,6 +95,70 @@ public class NotifyServletTest {
         
         
         verify(wikiProvider).pullChanges(account, directory, BRANCH);
+    }
+    
+    /*
+    * tests with two sessions. The expected behaviour is that pullChanges will be called
+    * only once since both sessions link to the same wiki.
+    */
+    @Test
+    public void testNotifyGetSameDirectory() throws IOException, ServletException, GitAPIException {
+
+        when(request.getParameter("id")).thenReturn(REPOSITORY);
+        when(request.getMethod()).thenReturn("GET");
+        
+        Set<String> attributes = ImmutableSet.of(ACCOUNT, "servlet."+REPOSITORY+"/"+BRANCH);
+        Enumeration<String> attributesEnum = java.util.Collections.enumeration(attributes);
+        when(session2.getAttributeNames()).thenReturn(attributesEnum);
+        when(session2.getAttribute(ACCOUNT)).thenReturn(account);
+        
+        File directory = new File("home",(REPOSITORY+"/"+BRANCH));
+        when(wikiProvider.getRepositoryDirectory(REPOSITORY+"/"+BRANCH)).thenReturn(directory);
+
+        servlet.service(request, response);
+        
+        
+        verify(wikiProvider).pullChanges(account, directory, BRANCH);
+    }
+    
+    /*
+    * tests with two sessions. The expected behaviour is that pullChanges will be called
+    * twice since we got two different branches for the called REPOSITORY.
+    */
+    @Test
+    public void testNotifyGetDifferentBranches() throws IOException, ServletException, GitAPIException {
+        when(request.getParameter("id")).thenReturn(REPOSITORY);
+        when(request.getMethod()).thenReturn("GET");
+        
+        Set<String> attributes = ImmutableSet.of(ACCOUNT, "servlet."+REPOSITORY+"/"+BRANCH2);
+        Enumeration<String> attributesEnum = java.util.Collections.enumeration(attributes);
+        when(session2.getAttributeNames()).thenReturn(attributesEnum);
+        when(session2.getAttribute(ACCOUNT)).thenReturn(account);
+        
+        File directory = new File("home",(REPOSITORY+"/"+BRANCH));
+        when(wikiProvider.getRepositoryDirectory(REPOSITORY+"/"+BRANCH)).thenReturn(directory);
+
+        servlet.service(request, response);
+        
+        
+        verify(wikiProvider, times(2)).pullChanges(any(Account.class), any(File.class), anyString());
+    }
+    
+    /*
+    * tests with the wrong repository
+    */
+    @Test
+    public void testNotifyGetWrongRepository() throws IOException, ServletException, GitAPIException {
+        when(request.getParameter("id")).thenReturn(WRONG_REPOSITORY);
+        when(request.getMethod()).thenReturn("GET");
+        
+        File directory = new File("home",(REPOSITORY+"/"+BRANCH));
+        when(wikiProvider.getRepositoryDirectory(REPOSITORY+"/"+BRANCH)).thenReturn(directory);
+
+        servlet.service(request, response);
+        
+        
+        verify(wikiProvider, times(0)).pullChanges(any(Account.class), any(File.class), anyString());
     }
     
 }
