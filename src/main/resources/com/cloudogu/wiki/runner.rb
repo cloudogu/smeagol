4660fee664ca6d904ec9681cf1df6b0a605a163f
Sebastian Sdorra <sebastian.sdorra@triology.de>
2017-08-01 09:59:08 +0200

    #19 reduce supported formats to markdown, asciidoc, creole, plain and rdoc

--- src/main/resources/com/cloudogu/wiki/runner.rb
+++ src/main/resources/com/cloudogu/wiki/runner.rb
@@ -10,6 +10,14 @@ GitHub::Markup::Markdown::MARKDOWN_GEMS['kramdown'] = proc { |content|
   Kramdown::Document.new(content, :auto_ids => false).to_html
 }
 
+# remove non used markup formats
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+Gollum::Markup.formats.delete(:org)
+Gollum::Markup.formats.delete(:rest)
+Gollum::Markup.formats.delete(:mediawiki)
+Gollum::Markup.formats.delete(:pod)
+Gollum::Markup.formats.delete(:textile)
+
 module Precious
   class App < Sinatra::Base
 
