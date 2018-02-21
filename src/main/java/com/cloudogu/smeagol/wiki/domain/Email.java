4d0294211e225b5c6514f0659bdc35732b1b2200
Sebastian Sdorra <s.sdorra@gmail.com>
2018-02-21 08:03:59 +0100

    turn todo into an question issue, see #41

--- src/main/java/com/cloudogu/smeagol/wiki/domain/Email.java
+++ src/main/java/com/cloudogu/smeagol/wiki/domain/Email.java
@@ -10,7 +10,6 @@ import java.util.regex.Pattern;
  */
 public final class Email {
 
-    // TODO should we use such a strong validation?
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
         "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
     );
