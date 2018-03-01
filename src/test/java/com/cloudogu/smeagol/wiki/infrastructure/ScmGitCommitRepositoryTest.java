2bea217a84c167e8a29660884249e6dd2ddfec3e
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2018-03-01 14:30:36 +0100

    #50 fix broken test

--- src/test/java/com/cloudogu/smeagol/wiki/infrastructure/ScmGitCommitRepositoryTest.java
+++ src/test/java/com/cloudogu/smeagol/wiki/infrastructure/ScmGitCommitRepositoryTest.java
@@ -4,21 +4,31 @@ import com.cloudogu.smeagol.wiki.domain.Commit;
 import com.cloudogu.smeagol.wiki.domain.History;
 import com.cloudogu.smeagol.wiki.domain.Path;
 import com.cloudogu.smeagol.wiki.domain.WikiId;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.cloudogu.smeagol.wiki.DomainTestData.COMMIT;
-import static com.cloudogu.smeagol.wiki.DomainTestData.PATH_HOME;
+import java.io.File;
+import java.util.Arrays;
+
 import static com.cloudogu.smeagol.wiki.DomainTestData.WIKI_ID_42;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ScmGitCommitRepositoryTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @Mock
     private GitClientProvider gitClientProvider;
@@ -36,16 +46,28 @@ public class ScmGitCommitRepositoryTest {
 
     @Test
     public void testFindHistoryByWikiIdAndPath() throws Exception {
-        WikiId id = WIKI_ID_42;
-        Path path = PATH_HOME;
-        Commit commit = COMMIT;
+        File repository = temporaryFolder.newFolder();
+        try (Git git = Git.init().setDirectory(repository).call()) {
+            File file = new File(repository, "Home");
+            Files.write("my content", file, Charsets.UTF_8);
 
-        History history = commitRepository.findHistoryByWikiIdAndPath(id ,path);
-        assertEquals(id, history.getWikiId());
-        assertEquals(path, history.getPath());
+            git.add().addFilepattern("Home.md").call();
+            RevCommit revCommit = git.commit()
+                    .setMessage("sample commit")
+                    .setAuthor("Tricia McMillian", "trillian@hitchhiker.com")
+                    .call();
+            when(gitClient.findCommits("Home.md")).thenReturn(Arrays.asList(revCommit));
 
-        Commit commitFromHistory = history.getCommits().get(0);
-        assertEquals(commit, commitFromHistory);
+            WikiId id = WIKI_ID_42;
+            Path path = Path.valueOf("Home");
+
+            History history = commitRepository.findHistoryByWikiIdAndPath(id, path);
+            assertEquals(id, history.getWikiId());
+            assertEquals(path, history.getPath());
+
+            Commit commitFromHistory = history.getCommits().get(0);
+            assertEquals("trillian@hitchhiker.com", commitFromHistory.getAuthor().getEmail().getValue());
+        }
     }
 
 }
