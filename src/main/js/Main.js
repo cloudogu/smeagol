a0e9fe9143abb37084559c785269b2ff5ad813f7
Maren Süwer <maren.suewer@triology.de>
2018-03-07 16:55:41 +0100

    add routing for history view

--- src/main/js/Main.js
+++ src/main/js/Main.js
@@ -11,6 +11,8 @@ import WikiRoot from './wiki/containers/WikiRoot';
 import Page from './wiki/containers/Page';
 import {Switch} from 'react-router-dom';
 import Directory from './wiki/containers/Directory';
+import History from './wiki/containers/History';
+
 
 const styles = {
     content: {
@@ -33,6 +35,7 @@ class Main extends React.Component<Props> {
                 <Route exact path="/:repository" component={Branches} />
                 <Route exact path="/:repository/:branch" component={WikiRoot} />
                 <Route path="/:repository/:branch/pages" component={Directory} />
+                <Route path="/:repository/:branch/history" component={History}/>
                 <Route path="/:repository/:branch" component={Page} />
                 </Switch>
             </div>
