ec09de0f39c3b08ca0924477c12b19091c857df1
Maren Süwer <maren.suewer@cloudogu.com>
2018-05-29 12:28:20 +0200

    add tests for timestamp in wiki

--- src/main/js/wiki/modules/wiki.test.js
+++ src/main/js/wiki/modules/wiki.test.js
@@ -10,11 +10,16 @@ it('shouldFetchWiki without wiki entry', () => {
 });
 
 it('shouldFetchWiki with loading entry', () => {
-    const state = { wiki: { '42@master': { loading: true } } };
+    const state = { wiki: { '42@master': { loading: true, timestamp: Date.now() } } };
     expect(shouldFetchWiki(state, '42', 'master')).toBeFalsy();
 });
 
 it('shouldFetchWiki with wiki entry', () => {
     const state = { wiki: { '42@master': { loading: false, wiki: {} } } };
     expect(shouldFetchWiki(state, '42', 'master')).toBeFalsy();
+});
+
+it('shouldFetchWiki is already loaded but timestamp consideres to load again', () => {
+    const state = { wiki: { '42@master': { loading: true, timestamp: (Date.now()-10005) } } };
+    expect(shouldFetchWiki(state, '42', 'master')).toBeTruthy();
 });
