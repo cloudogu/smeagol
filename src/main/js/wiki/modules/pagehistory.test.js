2bd8a0f5e85be906542796c8c9d533b1eff24d8c
Maren Süwer <maren.suewer@cloudogu.com>
2018-05-29 15:10:24 +0200

    add tests

---
+++ src/main/js/wiki/modules/pagehistory.test.js
@@ -0,0 +1,32 @@
+import {shouldFetchHistory} from './pagehistory';
+
+it('shouldFetchHistory without directory entry', () => {
+    const state = { pagehistory: { } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeTruthy();
+});
+
+it('shouldFetchHistory with loading entry', () => {
+    const state = { pagehistory: { 'docs/Home': { loading: true, timestamp: Date.now() } } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchHistory with notFound entry', () => {
+    const state = { pagehistory: { 'docs/Home': { notFound: true } } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchHistory with error entry', () => {
+    const state = { pagehistory: { 'docs/Home': { error: new Error('something went wrong') } } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchHistory with wiki entry', () => {
+    const state = { pagehistory: { 'docs/Home': { loading: false } } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeTruthy();
+});
+
+
+it('shouldFetchHistory with expired timestamp', () => {
+    const state = { pagehistory: { 'docs/Home': { loading: true, timestamp: (Date.now()-10005) } } };
+    expect(shouldFetchHistory(state, 'docs/Home')).toBeTruthy();
+});
---
+++ src/main/js/wiki/modules/search.test.js
@@ -0,0 +1,32 @@
+import {shouldFetchSearchResults} from './search';
+
+it('shouldFetchSearchResults without directory entry', () => {
+    const state = { search: { } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeTruthy();
+});
+
+it('shouldFetchSearchResults with loading entry', () => {
+    const state = { search: { 'docs/Home': { loading: true, timestamp: Date.now() } } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchSearchResults with notFound entry', () => {
+    const state = { search: { 'docs/Home': { notFound: true } } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchSearchResults with error entry', () => {
+    const state = { search: { 'docs/Home': { error: new Error('something went wrong') } } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeFalsy();
+});
+
+it('shouldFetchSearchResults with wiki entry', () => {
+    const state = { search: { 'docs/Home': { loading: false } } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeTruthy();
+});
+
+
+it('shouldFetchSearchResults with expired timestamp', () => {
+    const state = { search: { 'docs/Home': { loading: true, timestamp: (Date.now()-10005) } } };
+    expect(shouldFetchSearchResults(state, 'docs/Home')).toBeTruthy();
+});
