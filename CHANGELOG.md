6ee29b972baca10ab1e9dd2c28084fab0c919aa5
Joshua Sprey <joshua.sprey@cloudogu.com>
2020-12-09 11:35:36 +0100

    (#93) Adds the configuration entries for the MinRAM- and MaxRAMPercentage to the dogu.json. Adds a CHANGELOG.md to Smeagol.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+* the ability to configure the memory limit for the container via `cesapp edit-conf` (#93)
+* the ability to configure the `MaxRamPercentage` and `MinRamPercentage` for the Smeagol process inside the container via `cesapp edit-conf`  (#93)
--- ces-startup.sh
+++ ces-startup.sh
@@ -35,13 +35,17 @@ if [[ "$(doguctl config "container_config/memory_limit" -d "empty")" == "empty" 
        -Djavax.net.ssl.trustStorePassword=changeit \
        -jar /app/smeagol.war
 else
-  echo "Starting Smeagol with memory limits..."
+  # Retrieve configurable java limits from etcd, valid default values exist
+  MEMORY_LIMIT_MAX_PERCENTAGE=$(doguctl config "container_config/java_max_ram_percentage")
+  MEMORY_LIMIT_MIN_PERCENTAGE=$(doguctl config "container_config/java_min_ram_percentage")
+  echo "Starting Smeagol with memory limits: MaxRAMPercentage=${MEMORY_LIMIT_MAX_PERCENTAGE}, MinRAMPercentage=${MEMORY_LIMIT_MIN_PERCENTAGE} ..."
+
   java -Djava.awt.headless=true \
        -Djava.net.preferIPv4Stack=true \
        -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
        -Djavax.net.ssl.trustStorePassword=changeit \
-       -XX:MaxRAMPercentage=85.0 \
-       -XX:MinRAMPercentage=50.0 \
+       -XX:MaxRAMPercentage="${MEMORY_LIMIT_MAX_PERCENTAGE}" \
+       -XX:MinRAMPercentage="${MEMORY_LIMIT_MIN_PERCENTAGE}" \
        -jar /app/smeagol.war
 fi
 
