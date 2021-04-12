ee42f7a3462ac008941ea0d2feaf010a6611401d
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2021-04-12 09:43:26 +0200

    Fix unit tests to match new error handling #125

--- src/main/js/apiclient.test.ts
+++ src/main/js/apiclient.test.ts
@@ -1,4 +1,4 @@
-import { apiClient } from "./apiclient";
+import { apiClient, UNAUTHORIZED_ERROR } from "./apiclient";
 
 const DEFAULT_LOCATION = "http://localhost";
 
@@ -40,9 +40,10 @@ test("test apiClient.get", async () => {
 
 test("test apiClient.get with status 401 and without location header", async () => {
   mockFetchAndWindow(401);
-  const response = await apiClient.get("/api/v1/marvinctl");
 
-  expect(response.status).toBe(401);
+  await expect(() => {
+    return apiClient.get("/api/v1/marvinctl");
+  }).rejects.toThrow(UNAUTHORIZED_ERROR);
 
   // be sure no one has done a redirect
   expect(window.location.href).toBe(DEFAULT_LOCATION);
@@ -50,7 +51,10 @@ test("test apiClient.get with status 401 and without location header", async () 
 
 test("test ApiClient.get with status 401 and location header", async () => {
   mockFetchAndWindow(401, "/hitchhikers");
-  await apiClient.get("/api/v1/marvinctl");
+
+  await expect(() => {
+    return apiClient.get("/api/v1/marvinctl");
+  }).rejects.toThrow(UNAUTHORIZED_ERROR);
 
   expect(window.location.href).toBe("/api/v1/authc?location=http%3A%2F%2Flocalhost");
 });
