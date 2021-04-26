4b21fd4c89a04665bd3296c8d89b9aa1f5fb276a
Simon Klein <simon.klein@cloudogu.com>
2021-04-26 16:16:03 +0200

    #129 Fix regex

--- src/main/js/pathUtil.ts
+++ src/main/js/pathUtil.ts
@@ -10,6 +10,6 @@ export function isValidRelativePath(path: string): boolean {
   if (path === null || path === undefined || path.length === 0) {
     return false;
   }
-  const absolutePathRegex = /^[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?(\/[a-zA-Z0-9]([a-zA-Z0-9-]*[A-Z0-9])?)*$/g;
+  const absolutePathRegex = /^[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?(\/[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?)*$/g;
   return path.match(absolutePathRegex) !== null;
 }
