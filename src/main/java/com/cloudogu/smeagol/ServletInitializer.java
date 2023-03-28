8693af34850c7ef896234caf125ac10041e4c1d8
Simon Klein <simon.klein@cloudogu.com>
2023-03-28 17:37:45 +0200

    #207 Fix cves

--- src/main/java/com/cloudogu/smeagol/ServletInitializer.java
+++ src/main/java/com/cloudogu/smeagol/ServletInitializer.java
@@ -12,4 +12,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
         return application.sources(Smeagol.class);
     }
 
+    @Override
+    public void onStartup(jakarta.servlet.ServletContext servletContext){
+    }
 }
