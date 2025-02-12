c967f70a624139951fc0a5a2fa649caa8786b065
Simon Klein <simon.klein@cloudogu.com>
2025-02-12 11:16:20 +0100

    #238 Exit on invalid log level

--- resources/logging.sh
+++ resources/logging.sh
@@ -9,20 +9,14 @@ DEFAULT_LOGGING_KEY="logging/root"
 SMEAGOL_LOGGING_TEMPLATE="/app/logback.xml.tpl"
 SMEAGOL_LOGGING="/app/logback.xml"
 
-function validateDoguLogLevel() {
-  echo "Validate root log level"
+echo "Validate root log level"
+validateExitCode=0
+doguctl validate "${DEFAULT_LOGGING_KEY}" || validateExitCode=$?
 
-  validateExitCode=0
-  doguctl validate "${DEFAULT_LOGGING_KEY}" || validateExitCode=$?
-
-  if [[ ${validateExitCode} -ne 0 ]]; then
-      echo "WARNING: The loglevel configured in ${DEFAULT_LOGGING_KEY} is invalid."
-  fi
-
-  return
-}
-
-validateDoguLogLevel
+if [[ ${validateExitCode} -ne 0 ]]; then
+    echo "ERROR: The loglevel configured in ${DEFAULT_LOGGING_KEY} is invalid. Cannot start dogu..."
+    exit 1
+fi
 
 echo "Rendering logging configuration..."
 doguctl template ${SMEAGOL_LOGGING_TEMPLATE} ${SMEAGOL_LOGGING}
