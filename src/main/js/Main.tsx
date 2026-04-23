974bac3bf7977b0277b6c2902a92c535221ec4a2
Florian Scholdei <florian.scholdei@cloudogu.com>
2026-04-23 10:39:27 +0200

    #257 Bump node dependencies

--- src/main/js/Main.tsx
+++ src/main/js/Main.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import injectSheet from "react-jss";
 import classNames from "classnames";
 
-import { Route, withRouter } from "react-router";
+import { Route, withRouter, Switch } from "react-router-dom";
 
-import { Switch } from "react-router-dom";
 import Wiki from "./wiki/containers/Wiki";
 import RepositoryOverview from "./repository/containers/RepositoryOverview";
 
