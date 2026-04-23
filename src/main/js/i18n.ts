6fa81fd71b9871c23c2df5fc1001fe33485a61f0
Florian Scholdei <florian.scholdei@cloudogu.com>
2026-04-23 14:10:11 +0200

    #257 Update i18n integration

--- src/main/js/i18n.ts
+++ src/main/js/i18n.ts
@@ -1,27 +1,26 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { reactI18nextModule } from "react-i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslations from "./assets/locales/en/translations.json";
+import deTranslations from "./assets/locales/de/translations.json";
 
 import "dayjs/locale/de.js";
 
-// The import is essential for webpack to detect the translations and to pack them together with the bundle.js
-import resBundle from "i18next-resource-store-loader!./assets/locales/DoNotDelete";
-
 i18n
   .use(LanguageDetector)
-  .use(reactI18nextModule)
+  .use(initReactI18next)
   .init({
     fallbackLng: "en",
-
-    // have a common namespace used around the full app
     ns: ["translations"],
     defaultNS: "translations",
-    resources: resBundle,
-
+    resources: {
+      en: { translations: enTranslations },
+      de: { translations: deTranslations }
+    },
     debug: true,
-
-    react: {
-      wait: true
+    interpolation: {
+      escapeValue: false
     }
   });
 
