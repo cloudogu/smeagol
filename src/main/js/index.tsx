043ef00c375eca88793c4aaa8769d4d60e21febf
Florian Scholdei <florian.scholdei@cloudogu.com>
2026-04-24 14:55:53 +0200

    #257 Refactor history import

--- src/main/js/index.tsx
+++ src/main/js/index.tsx
@@ -6,15 +6,13 @@ import registerServiceWorker from "./registerServiceWorker";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
-import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory as createHistory } from "history";
 import { Router } from "react-router";
 
 // Create a history of your choosing (we're using a browser history in this case)
 const history = createHistory({
   basename: process.env.PUBLIC_URL
 });
-
-window.appHistory = history;
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
